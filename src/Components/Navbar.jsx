b89857782e07ad9879e3c210746bef77c1c10870
Megha2729-D <134476584+Megha2729-D@users.noreply.github.com>
2025-12-25 16:37:33 +0530

    navbar issue

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -24,18 +24,30 @@ const Navbar = () => {
     }, [showNavbar]);
 
     /* -------------------- DROPDOWNS -------------------- */
-    const toggleDropdown = (key) => {
-        setDropdownOpen(prev => ({
-            [key]: !prev[key]
-        }));
+    const toggleDropdown = (key, e) => {
+        e.stopPropagation();
+        setDropdownOpen(prev => {
+            const next = {};
+            Object.keys(prev).forEach(k => (next[k] = false));
+            next[key] = !prev[key];
+            return next;
+        });
         setSubDropdownOpen({});
     };
 
-    const toggleSubDropdown = (key) => {
+    const toggleSubDropdown = (key, e) => {
+        e.stopPropagation();
         setSubDropdownOpen(prev => ({
             [key]: !prev[key]
         }));
     };
+
+    const handleItemClick = () => {
+        setShowNavbar(false);
+        setDropdownOpen({});
+        setSubDropdownOpen({});
+    };
+
 
     const closeMenu = () => {
         setShowNavbar(false);
@@ -69,20 +81,119 @@ const Navbar = () => {
 
                         {/* NAV LINKS */}
                         <div className={`nav-elements ${showNavbar ? 'active' : ''}`}>
-                            <ul>
-                                <li onClick={() => toggleDropdown('self')}>
-                                    Self-paced Courses
+                            <ul className="mb-0 p-lg-0">
+
+                                {/* SELF-PACED */}
+                                <li
+                                    className={`dropdown ${dropdownOpen.selfPaced ? 'open' : ''}`}
+                                    onClick={(e) => toggleDropdown('selfPaced', e)}
+                                >
+                                    <span className="dropdown-toggle">
+                                        Self-paced Courses <i className="bi bi-chevron-down"></i>
+                                    </span>
+
+                                    <ul className="dropdown-menu" onClick={(e) => e.stopPropagation()}>
+
+                                        {/* GROUP 1 */}
+                                        <li className="sub-dropdown">
+                                            <span
+                                                className="sub-dropdown-toggle"
+                                                onClick={(e) => toggleSubDropdown('group1', e)}
+                                            >
+                                                Menu Group 1 <i className="bi bi-chevron-right"></i>
+                                            </span>
+
+                                            <ul className={`sub-dropdown-menu ${subDropdownOpen.group1 ? 'open' : ''}`}>
+                                                <li>
+                                                    <NavLink to="/practice/ide" onClick={handleItemClick}>
+                                                        Online IDE
+                                                    </NavLink>
+                                                </li>
+                                                <li>
+                                                    <NavLink to="/practice/debugging" onClick={handleItemClick}>
+                                                        Debugging
+                                                    </NavLink>
+                                                </li>
+                                            </ul>
+                                        </li>
+
+                                        {/* GROUP 2 */}
+                                        <li className="sub-dropdown">
+                                            <span
+                                                className="sub-dropdown-toggle"
+                                                onClick={(e) => toggleSubDropdown('group2', e)}
+                                            >
+                                                Menu Group 2 <i className="bi bi-chevron-right"></i>
+                                            </span>
+
+                                            <ul className={`sub-dropdown-menu ${subDropdownOpen.group2 ? 'open' : ''}`}>
+                                                <li>
+                                                    <NavLink to="/practice/challenges" onClick={handleItemClick}>
+                                                        Challenges
+                                                    </NavLink>
+                                                </li>
+                                                <li>
+                                                    <NavLink to="/practice/projects" onClick={handleItemClick}>
+                                                        Projects
+                                                    </NavLink>
+                                                </li>
+                                            </ul>
+                                        </li>
+
+                                    </ul>
                                 </li>
-                                <li onClick={() => toggleDropdown('live')}>
-                                    Live Courses
+
+                                {/* LIVE COURSES */}
+                                <li
+                                    className={`dropdown ${dropdownOpen.liveCourses ? 'open' : ''}`}
+                                    onClick={(e) => toggleDropdown('liveCourses', e)}
+                                >
+                                    <span className="dropdown-toggle">
+                                        Live Courses <i className="bi bi-chevron-down"></i>
+                                    </span>
+
+                                    <ul className="dropdown-menu" onClick={(e) => e.stopPropagation()}>
+                                        <li><NavLink to="/" onClick={handleItemClick}>UI UX Design</NavLink></li>
+                                        <li><NavLink to="/" onClick={handleItemClick}>Data Science</NavLink></li>
+                                        <li><NavLink to="/" onClick={handleItemClick}>Full Stack Web Development</NavLink></li>
+                                        <li><NavLink to="/" onClick={handleItemClick}>Python Full Stack</NavLink></li>
+                                    </ul>
                                 </li>
-                                <li onClick={() => toggleDropdown('practice')}>
-                                    Practice
+
+                                {/* PRACTICE */}
+                                <li
+                                    className={`dropdown ${dropdownOpen.practice ? 'open' : ''}`}
+                                    onClick={(e) => toggleDropdown('practice', e)}
+                                >
+                                    <span className="dropdown-toggle">
+                                        Practice <i className="bi bi-chevron-down"></i>
+                                    </span>
+
+                                    <ul className="dropdown-menu" onClick={(e) => e.stopPropagation()}>
+                                        <li><NavLink to="/practice/ide" onClick={handleItemClick}>Online IDE</NavLink></li>
+                                        <li><NavLink to="/practice/debugging" onClick={handleItemClick}>Debugging</NavLink></li>
+                                        <li><NavLink to="/practice/challenges" onClick={handleItemClick}>Challenges</NavLink></li>
+                                    </ul>
                                 </li>
-                                <li onClick={() => toggleDropdown('resources')}>
-                                    Resources
+
+                                {/* RESOURCES */}
+                                <li
+                                    className={`dropdown ${dropdownOpen.resources ? 'open' : ''}`}
+                                    onClick={(e) => toggleDropdown('resources', e)}
+                                >
+                                    <span className="dropdown-toggle">
+                                        Resources <i className="bi bi-chevron-down"></i>
+                                    </span>
+
+                                    <ul className="dropdown-menu" onClick={(e) => e.stopPropagation()}>
+                                        <li><NavLink to="/resources/blogs" onClick={handleItemClick}>Blogs</NavLink></li>
+                                        <li><NavLink to="/resources/docs" onClick={handleItemClick}>Docs</NavLink></li>
+                                        <li><NavLink to="/resources/tools" onClick={handleItemClick}>Tools</NavLink></li>
+                                    </ul>
                                 </li>
+
                             </ul>
+
                         </div>
 
                         {/* RIGHT */}
