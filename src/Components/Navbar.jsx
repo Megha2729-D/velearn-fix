0716ff8ca4cd01992d288da1ba86d636a1f64a2e
Megha2729-D <134476584+Megha2729-D@users.noreply.github.com>
2025-12-25 13:50:26 +0530

    navbar issue

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -47,11 +47,7 @@ const Navbar = () => {
         <nav className={`v-navbar ${scrolled ? 'scrolled' : ''}`}>
             {/* TOP BANNER */}
             <div className="top-banner">
-                <div className="bg-c1 text-white py-1 d-flex align-items-center overflow-hidden">
-                    <div className="w-100">
-                        <p className='mb-0 text-center py-1'> New batch offer live. Start your IT journey now.</p>
-                    </div>
-                </div>
+                <p>New batch offer live. Start your IT journey now.</p>
             </div>
 
             {/* MAIN NAV */}
@@ -87,35 +83,12 @@ const Navbar = () => {
                             </ul>
                         </div>
 
-                        {/* Right Section */}
-                        <div className="d-flex gap-4 right_nav_icons">
-                            <div className="d-lg-flex d-none align-items-center">
-                                <div className="search_parent position-relative">
-                                    <div className="d-flex align-items-center">
-                                        <i className="bi bi-search"></i>
-                                        <input type="search" placeholder="Search" />
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className="d-lg-flex d-none gap-2 text-white">
-                                <span className="d-none d-lg-block">
-                                    <Link to="/login" className="btn_login">Login</Link>
-                                </span>
-                                <span className="d-none d-lg-block">|</span>
-                                <span>
-                                    <Link to="/signup" className="btn_signup">Sign Up</Link>
-                                </span>
-                            </div>
-                            <div className='d-flex d-lg-none nav_mbl_icons'>
-                                <div className='pe-3 d-flex align-items-center'>
-                                    <i className="bi bi-search"></i>
-                                </div>
-                                <span>
-                                    <Link to="/login" className="btn_login">Login</Link>
-                                </span>
-                            </div>
+                        {/* RIGHT */}
+                        <div className="right_nav_icons">
+                            <Link to="/login" className="btn_login">Login</Link>
+                            <Link to="/signup" className="btn_signup">Sign Up</Link>
                         </div>
+
                     </div>
                 </div>
             </div>
